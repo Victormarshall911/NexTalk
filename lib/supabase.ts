0729219b9f1fdb0685ab470689f0befd65e8e390
Victormarshall911 <favourjames1977@gmail.com>
2026-01-17 12:59:30 +0100

    Updated supabase request

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,9 +4,30 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
+const ExpoWebStorage = {
+  getItem: (key: string) => {
+    if (typeof window === 'undefined') {
+      return Promise.resolve(null);
+    }
+    return AsyncStorage.getItem(key);
+  },
+  setItem: (key: string, value: string) => {
+    if (typeof window === 'undefined') {
+      return Promise.resolve(); // Returns a Promise that resolves to void
+    }
+    return AsyncStorage.setItem(key, value);
+  },
+  removeItem: (key: string) => {
+    if (typeof window === 'undefined') {
+      return Promise.resolve(); // Returns a Promise that resolves to void
+    }
+    return AsyncStorage.removeItem(key);
+  },
+};
+
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
-    storage: AsyncStorage,
+    storage: ExpoWebStorage,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
